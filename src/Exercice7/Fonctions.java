963ff2ba430fada60dcac0ff60cc1762241ce6ab
benjamin loge <benjy.loge@gmail.com>
2018-11-26 15:34:15 +0100

    Fonctions factorielle

--- src/Exercice7/Fonctions.java
+++ src/Exercice7/Fonctions.java
@@ -4,9 +4,23 @@ public class Fonctions {
 	
 	Fonctions ()
 	{
+
+	}
+	
+	
+	double factorielle(int x)  // x! = valeur
+	{
+		double valeur = 1;    //valeur initial de x! pour 0!
+		for(int i=1;i<=x;i++) {      // addition de 1*2*3*4*.....*x
+			valeur = valeur * i;	
+		}
+		return valeur;  
 		
 	}
 	
-	// Test
+	public static void main(String[] args) {
+		System.out.println(factorielle(5));
+		
+	}
 
 }
