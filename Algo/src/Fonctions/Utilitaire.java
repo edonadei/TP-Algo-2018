03180b065eabf520900d0036c6b1ed586c22ff80
benjamin loge <benjy.loge@gmail.com>
2018-11-26 16:07:21 +0100

    Exercice 7 fin

--- Algo/src/Fonctions/Utilitaire.java
+++ Algo/src/Fonctions/Utilitaire.java
@@ -1,7 +1,46 @@
 package Fonctions;
 
 public class Utilitaire {
+	
+	void Fonctions ()
+	{
 
+	}
 	
 	
+	static double factorielle(int x)  // x! = valeur
+	{
+		double valeur = 1;    //valeur initial de x! pour 0!
+		for(int i=1;i<=x;i++) {      // addition de 1*2*3*4*.....*x
+			valeur = valeur * i;	
+		}
+		return valeur;  
+		
+	}
+	
+	static double puissance(int nombre,int lapuissance) {
+		double valeur = 1;
+		for(int i = 0;i<lapuissance;i++) {
+			valeur = valeur * nombre;
+		}
+		return valeur;
+	}
+	
+	
+	
+	static double exponentiel(int a) {
+		double valeur = 0;
+		for(int i=0;i<=a;i++) {
+			valeur = valeur + puissance(a,i)/factorielle(i);
+		}
+		return valeur;
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(exponentiel(110));
+		
+	}
+
 }
